739307ded77c578f622c1d72b791799220313e04
Dima Prokopiuk <dima.prokopyuk@gmail.com>
2018-07-02 17:41:19 +0300

    Configure webpack to support scss

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 const webpack = require('webpack');
 const paths = require('./config/paths');
+const autoprefixer = require('autoprefixer');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 
 
 module.exports = {
@@ -13,17 +16,49 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ['babel-loader']
+        loader: require.resolve('babel-loader')
       },
       {
         enforce: 'pre',
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        loader: 'eslint-loader',
+        loader: require.resolve('eslint-loader'),
         options: {
           failOnWarning: true,
           failOnError: true,
         },
+      },
+      {
+        test: /\.(scss)$/,
+        use: [
+          require.resolve('style-loader'),
+          require.resolve('css-hot-loader'),
+          MiniCssExtractPlugin.loader,
+          {
+            loader: require.resolve('css-loader'),
+            options: {
+              importLoaders: 1
+            },
+          },
+          require.resolve('sass-loader'),
+          {
+            loader: require.resolve('postcss-loader'),
+            options: {
+              ident: 'postcss',
+              plugins: () => [
+                autoprefixer({
+                  browsers: [
+                    '>1%',
+                    'last 4 versions',
+                    'Firefox ESR',
+                    'not ie < 9', // React doesn't support IE8
+                  ],
+                  flexbox: 'no-2009',
+                })
+              ],
+            },
+          },
+        ],
       },
     ]
   },
@@ -31,7 +66,10 @@ module.exports = {
     extensions: ['*', '.js', '.jsx']
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.HotModuleReplacementPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'style.css'
+    }),
   ],
   devServer: {
     historyApiFallback: true,
