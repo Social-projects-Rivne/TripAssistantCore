29eba935b5f28d266f7ca8c534f17cd30d3a6e59
Dima Prokopiuk <dima.prokopyuk@gmail.com>
2018-07-07 15:56:23 +0300

    Changed the nesting of components

--- client/App.jsx
+++ client/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Routes from './routes';
 import Header from './components/Header';
@@ -25,16 +25,18 @@ class App extends Component {
   render() {
     const { sidebarActive } = this.state;
     return (
-      <div className="app">
-        <Menu sidebar={this.toggleSidebar} />
-        {sidebarActive && <Sidebar />}
+      <Fragment>
+        <Header />
         <div className="main">
-          <Header />
-          <Switch>
-            {Routes.map((route, i) => <Route {...route} key={i} />)}
-          </Switch>
+          <Menu sidebar={this.toggleSidebar} />
+          {sidebarActive && <Sidebar />}
+          <div className="content">
+            <Switch>
+              {Routes.map((route, i) => <Route {...route} key={i} />)}
+            </Switch>
+          </div>
         </div>
-      </div>
+      </Fragment>
     );
   }
 }
--- client/components/Header/Header.jsx
+++ client/components/Header/Header.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Logo from './components/Logo';
 import Authorization from './components/Authorization';
 import './Header.scss';
 
 const Header = () => (
   <header className="header">
+    <Logo />
     <Authorization />
   </header>
 );
