a4e72f623fd07a33b4f48c54fd3297befc0b98c1
RVCLASS05\Oleksii <iordatiy1991@gmail.com>
2018-07-09 14:42:24 +0300

    added materialize scss first stage

--- client/index.js
+++ client/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import 'materialize-css';
 import './index.scss';
-
 
 ReactDOM.render(
   <BrowserRouter>
