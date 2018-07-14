c592fffc02dbb90e0a2982afcd0543172c61e3b1
Dima Prokopiuk <dima.prokopyuk@gmail.com>
2018-07-14 23:54:19 +0300

    Fix header in sidebar

--- client/components/Sidebar/Sidebar.jsx
+++ client/components/Sidebar/Sidebar.jsx
@@ -1,19 +1,63 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Sidebar.scss';
 
 import TripInfo from './components/TripInfo';
 import TripPoint from './components/TripPoint';
+
 
 const POINT = [
   { point: 'A' },
   { point: 'B' }
 ];
 
-const Sidebar = () => (
-  <div className="sidebar">
-    <TripInfo />
-    { POINT.map(({ point }, i) => <TripPoint point={point} key={i} />) }
-  </div>
-);
+
+class Sidebar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      tripName: 'New Trip',
+      tripDuration: undefined,
+      tripTime: undefined,
+      tripFuel: undefined
+    };
+
+    this.eventChangeName = this.eventChangeName.bind(this);
+  }
+
+  eventChangeName({ currentTarget: { textContent } }) {
+    this.setState({ tripName: textContent });
+  }
+
+  render() {
+    const {
+      tripName,
+      tripDuration,
+      tripTime,
+      tripFuel
+    } = this.state;
+
+    return (
+      <div>
+        <div className="sidebar">
+          <div className="sidebar__header">
+            <TripInfo
+              className="red accent-1"
+              fun={this.eventChangeName}
+              name={tripName}
+              duration={tripDuration}
+              time={tripTime}
+              fuel={tripFuel}
+            />
+            <div className="trip-info_radius">
+              <input className="trip-info_radius_input" type="range" min="0" max="100" />
+            </div>
+          </div>
+          { POINT.map(({ point }, i) => <TripPoint point={point} key={i} />) }
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Sidebar;
--- client/components/Sidebar/components/TripInfo/TripInfo.jsx
+++ client/components/Sidebar/components/TripInfo/TripInfo.jsx
@@ -1,18 +1,45 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './TripInfo.scss';
 
-const TripInfo = () => (
-  <div className="trip-info">
-    <h2 className="trip-info_title">New Trip</h2>
+const colors = [
+  'red',
+  'pink',
+  'purple',
+  'blue',
+  'teal',
+  'light-green',
+  'lime',
+  'orange'
+];
+
+const random = (min, max) => Math.round(min + Math.random() * (max - min));
+
+const TripInfo = ({
+  name, duration, time, fuel, onSave
+}) => (
+  <div className={`trip-info ${colors[random(0, colors.length)]} accent-1`}>
+    <h5 contentEditable="true" suppressContentEditableWarning="true" className="trip-info_title" onBlur={onSave}>{name}</h5>
     <div className="trip-info_icon">
-      <span>200km</span>
-      <span>3:30</span>
-      <span>$50</span>
-    </div>
-    <div className="trip-info_radius">
-      <input className="trip-info_radius_input" type="range" min="0" max="100" />
+      <span>{duration} km</span>
+      <span>{time} h</span>
+      <span>$ {fuel}</span>
     </div>
   </div>
 );
+
+TripInfo.propTypes = {
+  name: PropTypes.string.isRequired,
+  duration: PropTypes.string,
+  time: PropTypes.string,
+  fuel: PropTypes.string,
+  onSave: PropTypes.func.isRequired
+};
+
+TripInfo.defaultProps = {
+  duration: '0',
+  time: '0',
+  fuel: '0'
+};
 
 export default TripInfo;
