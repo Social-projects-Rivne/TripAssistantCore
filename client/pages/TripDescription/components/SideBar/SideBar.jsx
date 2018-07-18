25669178284c5f130d2f2fec015e5628b40067bf
Stan0men <stan0men@yahoo.com>
2018-07-18 20:37:33 +0300

    trouble with hide panel -_-

--- client/pages/TripDescription/components/SideBar/SideBar.jsx
+++ client/pages/TripDescription/components/SideBar/SideBar.jsx
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import './SideBar.scss';
 
+
 class SideBar extends Component {
+  constructor() {
+    super();
+    const style = {
+      display: 'none'
+    };
+  }
+
   render() {
     return (
       <div className="sideBar">
-        <div id="panel"> {'<'}
+        <div id="panel" style={this.style.display}> {'<'}
           <div id="hidden_panel">Щось</div>
         </div>
       </div>
