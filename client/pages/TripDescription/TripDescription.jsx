0f3359e55cf80e4444b6ea8cbbceff113a8407bc
Stan0men <stan0men@yahoo.com>
2018-07-30 20:13:41 +0300

    add clearInput and filter methods

--- client/pages/TripDescription/TripDescription.jsx
+++ client/pages/TripDescription/TripDescription.jsx
@@ -2,25 +2,44 @@ import React, { Component } from 'react';
 import './TripDescription.scss';
 import feedbacksData from '../../../public/data/feedbacksData.json';
 
+function searchingFor(term) {
+  return function low(feed) {
+    return feed.name.toLowerCase().includes(term.toLowerCase()) || !term;
+  };
+}
+
 class TripDescription extends Component {
   constructor() {
     super();
     this.state = {
-      feedbackData: feedbacksData
+      feedbackData: feedbacksData,
+      term: ''
     };
+    this.searchHandler = this.searchHandler.bind(this);
+  }
+
+  resetHandler = () => {
+    this.setState({ term: '' });
+  }
+
+  searchHandler = (event) => {
+    this.setState({ term: event.target.value });
   }
 
   render() {
-    const { feedbackData } = this.state;
+    const { feedbackData, term } = this.state;
     return (
       <div>
         <div className="search">
-          <form onSubmit={this.clearInput}>
+          <form>
             <input
               type="text"
+              onChange={this.searchHandler}
+              value={term}
             />
             <button
               type="submit"
+              onClick={this.resetHandler}
             >
               <a href="# " className="waves-effect waves-light btn clear">CLEAR</a>
             </button>
@@ -28,7 +47,12 @@ class TripDescription extends Component {
         </div>
         <div className="feedbacksData">
           {
-            feedbackData.map(feed => <div key={feed.id}><h3>{feed.name}</h3></div>)
+            feedbackData.filter(searchingFor(term)).map(feed => (
+              <div key={feed.id} className="data">
+                <h4>{feed.name}</h4>
+                <p>Rating: {feed.rating}</p>
+              </div>
+            ))
           }
         </div>
       </div>
