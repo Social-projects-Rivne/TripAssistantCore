2aba372df4a616da253c873fa90b5d6386a5867a
Stan0men <stan0men@yahoo.com>
2018-07-31 11:23:25 +0300

    add some styles to Info page

--- client/pages/Info/Info.jsx
+++ client/pages/Info/Info.jsx
@@ -4,11 +4,12 @@ import './Info.scss';
 
 const Info = () => (
   <div className="main-info">
-    <div className="Map">
-      <p>Here will be map</p>
+    <div className="team">
+      <h1>Hi we are team RV-034.WebUI\Node.js</h1>
+      <p>We are hope that you have enjoyed to use our Trip-assistant</p>
     </div>
     <div className="contacts">
-      <h1>Contacts</h1>
+      <h1>Contact us!</h1>
       <p>Our phone 66-666-666</p>
       <p>Our email ad@666.gmail.com</p>
     </div>
@@ -20,11 +21,14 @@ const Info = () => (
         <a href="# " className="waves-effect waves-light btn-large"><i className="material-icons left">Submit</i></a>
       </form>
     </div>
+    <div className="map">
+      <p>Here will be map</p>
+    </div>
     <footer>
       <div className="copyright">Copyright</div>
       <div className="footer-menu">
         <ul>
-          <li><a href="# ">icon</a></li>
+          <li><a href="# "><img src="./img/icons8-slack-48.png" alt="slack" /></a></li>
           <li><a href="# ">icon</a></li>
           <li><a href="# ">icon</a></li>
           <li><a href="# ">icon</a></li>
