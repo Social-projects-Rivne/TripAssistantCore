d1341dede1f342ace2370bd1892e920cd9b36dca
Stan0men <stan0men@yahoo.com>
2018-08-04 12:17:26 +0300

    accidentally commit from info_page, fix it

--- client/pages/Info/Info.jsx
+++ client/pages/Info/Info.jsx
@@ -4,12 +4,11 @@ import './Info.scss';
 
 const Info = () => (
   <div className="main-info">
-    <div className="team">
-      <h1>Hi we are team RV-034.WebUI\Node.js</h1>
-      <p>We are hope that you have enjoyed to use our Trip-assistant</p>
+    <div className="Map">
+      <p>Here will be map</p>
     </div>
     <div className="contacts">
-      <h1>Contact us!</h1>
+      <h1>Contacts</h1>
       <p>Our phone 66-666-666</p>
       <p>Our email ad@666.gmail.com</p>
     </div>
@@ -21,14 +20,11 @@ const Info = () => (
         <a href="# " className="waves-effect waves-light btn-large"><i className="material-icons left">Submit</i></a>
       </form>
     </div>
-    <div className="map">
-      <p>Here will be map</p>
-    </div>
     <footer>
       <div className="copyright">Copyright</div>
       <div className="footer-menu">
         <ul>
-          <li><a href="# "><img src="./img/icons8-slack-48.png" alt="slack" /></a></li>
+          <li><a href="# ">icon</a></li>
           <li><a href="# ">icon</a></li>
           <li><a href="# ">icon</a></li>
           <li><a href="# ">icon</a></li>
