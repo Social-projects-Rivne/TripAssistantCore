26d776e555741cc966adbbb2b7e3ca404904849e
Stan0men <stan0men@yahoo.com>
2018-08-05 18:40:02 +0300

    add map

--- client/pages/Info/Info.jsx
+++ client/pages/Info/Info.jsx
@@ -3,6 +3,7 @@ import './Info.scss';
 import slack from '../../../public/images/slack.svg';
 import facebook from '../../../public/images/facebook.svg';
 import github from '../../../public/images/github-logo.svg';
+import InfoMap from '../../components/InfoMap/InfoMap';
 
 
 const Info = () => (
@@ -14,26 +15,28 @@ const Info = () => (
     <div className="contacts">
       <h1>Contact us!</h1>
       <p>Our phone 66-666-666</p>
-      <p>Our email ad@666.gmail.com</p>
+      <p>Our email email@666.gmail.com</p>
     </div>
     <div className="form">
       <form>
-        <input type="text" name="name" placeholder="input your name\login" required /> <br />
-        <input type="email" name="email" placeholder="input your email" required /> <br />
-        <textarea placeholder="your message here" />
+        <input type="text" name="name" placeholder="your name\login" required /> <br />
+        <input type="email" name="email" placeholder="your email" required /> <br />
+        <textarea placeholder="your message here" required />
         <a href="# " className="waves-effect waves-light btn-large"><i className="material-icons left">Submit</i></a>
       </form>
     </div>
     <div className="map">
-      <p>Here will be map</p>
+      {
+        <InfoMap />
+      }
     </div>
     <footer>
-      <div className="copyright">Copyright</div>
+      <div className="copyright">Copyright &#169; by RV-034Web/UI</div>
       <div className="footer-menu">
         <ul>
-          <li><a href="# "><img src={slack} alt="slack" /></a></li>
-          <li><a href="# "><img src={facebook} alt="slack" /></a></li>
-          <li><a href="# "><img src={github} alt="slack" /></a></li>
+          <li><a href="https://rv-034webui.slack.com/ " rel="noopener noreferrer" target="_blank"><img src={slack} alt="slack" /></a></li>
+          <li><a href="https://uk-ua.facebook.com/SoftServeInc/ " rel="noopener noreferrer" target="_blank"><img src={facebook} alt="slack" /></a></li>
+          <li><a href="https://github.com/orgs/Social-projects-Rivne/teams/rv-034-webui/repositories " rel="noopener noreferrer" target="_blank"><img src={github} alt="slack" /></a></li>
         </ul>
       </div>
     </footer>
